12a101726f4e9302e194e46f558e3d0bf1c50efd
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2023-12-08 14:37:30 +0100

    feat: DOne the drawer screen a little

---
+++ screens/offer.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Offer = () => {
+  return (
+    <View>
+      <Text>Offer</Text>
+    </View>
+  );
+};
+
+export default Offer;
+
+const styles = StyleSheet.create({});
---
+++ screens/privay.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Privay = () => {
+  return (
+    <View>
+      <Text>Privay</Text>
+    </View>
+  );
+};
+
+export default Privay;
+
+const styles = StyleSheet.create({});
